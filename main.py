755197bbc4e73814a35ecd8b21fdc5d4d32eb42f
Gustavo Carnaval <gustavocarnaval@yahoo.com.br>
2021-02-01 17:21:18 -0300

    init

---
+++ main.py
@@ -0,0 +1,3 @@
+def insertusuario(nome, email, senha):
+    # finge que adicionou
+    return {"id": 1, "nome": nome}
---
+++ routes.py
@@ -0,0 +1,42 @@
+from flask import Flask, request
+from main import insertusuario
+
+
+app = Flask("Demo_Carnaval")
+
+
+@app.route("/olamundo", methods=["GET"])
+def olaMundo():
+    return{"ola": "mundo"}
+
+
+@app.route("/cadastra/usuario", methods=["POST"])
+def cadastra_usuario():
+    body = request.get_json()
+
+    if("nome" not in body):
+        return geraResponse(400, "O parametro nome é obrigatorio")
+
+    if("email" not in body):
+        return geraResponse(400, "O parametro email é obrigatorio")
+
+    if("senha" not in body):
+        return geraResponse(400, "O parametro senha é obrigatorio")
+
+    usuario = insertusuario(body["nome"], body["email"], body["senha"])
+
+    return geraResponse(200, "Usuario Criado", "user", usuario)
+
+
+def geraResponse(status, mensagem, nome_do_conteudo=False, conteudo=False):
+    response = {}
+    response["status"] = status
+    response["mensagem"] = mensagem
+
+    if(nome_do_conteudo and conteudo):
+        response[nome_do_conteudo] = conteudo
+
+    return response
+
+
+app.run()
