77747bfd6142812dbc767c0ac779eb73e6a85e51
Gustavo Carnaval <gustavocarnaval@yahoo.com.br>
2021-02-19 10:12:55 -0300

    init

--- main.py
+++ main.py
@@ -1,3 +1,59 @@
+import json
+import hashlib
+
+
 def insertusuario(nome, email, senha):
-    # finge que adicionou
-    return {"id": 1, "nome": nome}
+    # Inserir usuário no cadastro
+    # Read the users and pick de max ID
+    with open('data/users.json') as f:
+        df = json.load(f)
+    idmaxlist = max(df, key=lambda ev: ev['id'])
+    idmax = idmaxlist['id']
+    idnovo = idmax + 1
+
+    # Convert the inputs to a list with new id and append to data
+    entry = {'id': idnovo, 'nome': nome, 'email': email, 'senha': senha}
+    df.append(entry)
+
+    # Write de new json file with new data
+    with open('data/users.json', 'w') as json_file:
+        json.dump(df, json_file)
+
+    return {"id": idnovo, "nome": nome}
+
+
+def listusuario():
+    # Listar usuário no cadastro
+    # Read the users file
+    with open('data/users.json') as f:
+        df = json.load(f)
+    return df
+
+
+def check_login(username):
+    # Verificar usuário no login
+    # Read the logins and search for the username
+    with open('data/logins.json') as f:
+        df = json.load(f)
+    lt = [x for x in df if x['Login'] == username]
+    if len(lt) == 1:
+        sucesso = True
+        retorno = dict(sucesso=sucesso, resultado=lt[0]['Pass'])
+    elif len(lt) > 1:
+        sucesso = False
+        retorno = dict(sucesso=sucesso, resultado='Multiple')
+    else:
+        sucesso = False
+        retorno = dict(sucesso=sucesso, resultado='Not Found')
+    return retorno
+
+
+def check_password_hash(user_password, auth_password):
+    # Verificar senha no login
+    result = hashlib.md5(auth_password.encode())
+    converted = result.hexdigest()
+    if user_password == converted:
+        sucesso = True
+    else:
+        sucesso = False
+    return sucesso
--- routes.py
+++ routes.py
@@ -1,16 +1,103 @@
-from flask import Flask, request
-from main import insertusuario
+from flask import Flask, request, jsonify, make_response, Response
+import datetime
+import json
+import jwt
+from main import insertusuario, listusuario, check_login, check_password_hash
+from functools import wraps
+
+app = Flask("Demo_API_JWT")
+app.config['SECRET_KEY'] = 'thisisthesecretkey'
+app.config['JSON_AS_ASCII'] = False
 
 
-app = Flask("Demo_Carnaval")
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        # http://127.0.0.1:5000/route?token=aslasdjasd
+        # token = request.args.get('token')
+        # Token no header
+        token = request.headers['x-access-tokens']
+
+        if not token:
+            return jsonify({'message': 'Token is missing!'}), 401
+
+        try:
+            data = jwt.decode(
+                token, app.config['SECRET_KEY'], algorithms=["HS256"])
+        except:
+            return jsonify({'message': 'Token is invalid'}), 401
+
+        return f(*args, **kwargs)
+
+    return decorated
 
 
 @app.route("/olamundo", methods=["GET"])
 def olaMundo():
-    return{"ola": "mundo2"}
+    return{"ola": "mundo"}
 
 
-@app.route("/cadastra/usuario", methods=["POST"])
+@app.route("/login", methods=["GET"])
+def login():
+    auth = request.authorization
+
+    if not auth or not auth.username or not auth.password:
+        # Ou usuário ou senha ou ambos não preenchidos
+        return make_response('Could not verify!',
+                             401,
+                             {'WWW.Authentication': 'Basic realm: "Login and Pass Required"'})
+    else:
+        # Usuário e senha preenchidos
+        chk_user = check_login(auth.username)
+        if chk_user['sucesso']:
+            # Encontrou o usuário no cadastro
+            # Procura pela senha
+            snh = chk_user['resultado']
+            chk_pass = check_password_hash(snh, auth.password)
+            if chk_pass:
+                # Senha certa
+
+                # Controle de resposta:
+                codigo = 0
+                mensagem = 'Operação efetuada com sucesso.'
+                sucesso = True
+                # Datas de Criação e Validade:
+                dtCriacao = datetime.datetime.utcnow()
+                dtCriacaostr = dtCriacao.strftime('%Y-%m-%d %H:%M:%S')
+                dtValidade = dtCriacao + datetime.timedelta(hours=24)
+                dtValidadestr = dtValidade.strftime('%Y-%m-%d %H:%M:%S')
+                # Geração do Token:
+                token = jwt.encode(
+                    {'user': auth.username, 'exp': dtValidade}, app.config['SECRET_KEY'])
+                # Montagem do JSON de resposta:
+                jtoken = dict(accessToken=token, dtCriacao=dtCriacaostr,
+                              dtValidade=dtValidadestr)
+                resp = dict(codigo=codigo, mensagem=mensagem,
+                            sucesso=sucesso, resultado=jtoken)
+                # Resposta:
+                return Response(json.dumps(resp, ensure_ascii=False).encode('utf8'),  mimetype='application/json')
+
+            else:
+                # Senha errada
+                return make_response('Could not verify!',
+                                     401,
+                                     {'WWW.Authentication': 'Basic realm: "Password dont match"'})
+
+        else:
+            # Não Encontrou o usuário no cadastro ou múltiplos
+            return make_response('Could not verify!',
+                                 401,
+                                 {'WWW.Authentication': 'Basic realm: "Login não encontrado ou multiplos Logins"'})
+
+
+@ app.route("/listausuario", methods=["GET"])
+@ token_required
+def lista_usuario():
+    lt = listusuario()
+    return jsonify(lt)
+
+
+@ app.route("/cadastra/usuario", methods=["POST"])
 def cadastra_usuario():
     body = request.get_json()
 
