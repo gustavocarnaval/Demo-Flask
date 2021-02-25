e79689e540b13daa0336dd284c3e30c39e64ba22
Gustavo Carnaval <gustavocarnaval@yahoo.com.br>
2021-02-25 10:21:40 -0300

    gunicorn conf

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -3,10 +3,10 @@
 bind = "0.0.0.0:7000"
 workers = 3
 # Access log - records incoming HTTP requests
-accesslog = "/var/log/gunicorn.access.log"
+accesslog = "/var/log/gunicorn/gunicorn.access.log"
 # Error log - records Gunicorn server goings-on
-errorlog = "/var/log/gunicorn.error.log"
+errorlog = "/var/log/gunicorn/gunicorn.error.log"
 # Whether to send Nginx output to the error log 
 capture_output = True
 # How verbose the Gunicorn error logs should be 
-loglevel = "DEBUG"
+#loglevel = "DEBUG"
